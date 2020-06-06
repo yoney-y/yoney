c6794b95f8b583fec34afa0b7a237a06a3080dd1
yoney-y <yoni.yakova@abv.bg>
2020-06-06 18:02:14 +0300

    php

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Yoney</title>
+    <link rel="stylesheet" href="css/main.css">
+
+</head>
+
+<body>
+    <img src="portfolio/logo-final-01.png" alt="picture">
+   
+    <div class="container">
+        <h1>Yoney's desigs</h1>
+
+        <div class="grid">
+            <div class="grid-col grid-col--1">
+
+            </div>
+            <div class="grid-col grid-col--2">
+
+            </div>
+            <div class="grid-col grid-col--3">
+
+            </div>
+            <div class="grid-col grid-col--4">
+
+            </div>
+
+            <?php  
+                    
+                    for ($i = 0; $i < 10; $i++) {
+            ?>
+            
+                        <div class="grid-item">
+                        <div class="content">
+                        </div>
+                        <img src="portfolio/1.jpg" alt="picture">
+                        </div>
+                        <script>
+                    }
+            </script>
+
+            
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="portfolio/2 (3).jpg" alt="picture">
+            </div>
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="portfolio/2 (2).jpg" alt="picture">
+            </div>
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="portfolio/andrei-hod.jpg" alt="picture">
+            </div>
+
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="portfolio/apex-06.jpg">
+            </div>
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="portfolio/apex-07.jpg" alt="picture">
+            </div>
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="portfolio/apex-bold-03.jpg" alt="picture">
+            </div>
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/1_copy.jpg" alt="picture">
+            </div>
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/5.jpg" alt="picture">
+            </div>
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/4.jpg" alt="picture">
+            </div>
+
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/3_copy.jpg" alt="picture">
+            </div>
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/2_copy.jpg" alt="picture">
+            </div>
+          
+                      <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/6.jpg" alt="picture">
+            </div>
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/1_copy.jpg" alt="picture">
+            </div>
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/5.jpg" alt="picture">
+            </div>
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/4.jpg" alt="picture">
+            </div>
+
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/3_copy.jpg" alt="picture">
+            </div>
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/2_copy.jpg" alt="picture">
+            </div>
+          
+                      <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/6.jpg" alt="picture">
+            </div>
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/1_copy.jpg" alt="picture">
+            </div>
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/5.jpg" alt="picture">
+            </div>
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/4.jpg" alt="picture">
+            </div>
+
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/3_copy.jpg" alt="picture">
+            </div>
+
+            <div class="grid-item">
+                <div class="content">
+                    <h2>Airplane</h2>
+                </div>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/2_copy.jpg" alt="picture">
+            </div>
+
+        </div>
+    </div>
+   
+
+</body>
+<script src="https://unpkg.com/colcade@0/colcade.js"></script>
+<script>
+    var colc = new Colcade( '.grid', {
+        columns: '.grid-col',
+        items: '.grid-item'
+    });
+</script>
+</html>
